62868395d4a37d4a997521d49d65104e89212c84
Ricardo-Avila <54330605+Ricardo-Avila@users.noreply.github.com>
2024-05-28 13:28:19 -0600

    Update modificarBD.py

--- modificarBD.py
+++ modificarBD.py
@@ -74,14 +74,30 @@ def obtener_siguiente_id_ticket(conexion):
 def insertar_datos_usuarios(conexion, nombre, apellido, correo, telefono):
     try:
         cursor = conexion.cursor()
-        id=obtener_siguiente_id_usuario(conexion)
-        # Ejemplo de inserción de datos en la tabla USUARIOS
+        id = obtener_siguiente_id_usuario(conexion)
         sql_insert_query = """INSERT INTO USUARIOS (id, nombre, apellido, correo, telefono) 
                               VALUES (%s, %s, %s, %s, %s)"""
         valores = (id, nombre, apellido, correo, telefono)
         cursor.execute(sql_insert_query, valores)
         conexion.commit()
         print("Datos de usuario insertados correctamente")
+
+        # Enviar actualización a otros nodos
+        update_data = {
+            "action": "insert",
+            "data": {
+                "table": "usuarios",
+                "values": {
+                    "id": id,
+                    "nombre": nombre,
+                    "apellido": apellido,
+                    "correo": correo,
+                    "telefono": telefono
+                }
+            }
+        }
+        send_update(update_data)
+        print(f"Enviado mensaje de actualización: {update_data}")
 
     except mysql.connector.Error as error:
         print("Error al insertar datos de usuario:", error)
